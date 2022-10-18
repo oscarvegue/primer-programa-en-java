bfdae831e074ee28a888ba0c069eb01975789636
Oscar <oscarfuersuarez27@gmail.com>
2022-10-18 08:54:16 +0200

    2 commit

---
+++ Prueba3.java
@@ -0,0 +1,11 @@
+public class Prueba3 {  
+
+	public static void main(String args[]) {
+
+		System.out.println("20,2002");
+		System.out.println("pizza");
+		System.out.println("estopa");
+		
+	}
+
+}
